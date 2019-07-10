1310301b8454762a639a2eb6fed9e07b8b8fbc59
Balx0 <balx99@gmail.com>
2019-07-10 11:20:32 +0200

    new experiment tab

--- src/app/administration/new-experiment/new-experiment.component.ts
+++ src/app/administration/new-experiment/new-experiment.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
+
 
 @Component({
   selector: 'app-new-experiment',
   templateUrl: './new-experiment.component.html',
   styleUrls: ['./new-experiment.component.css']
 })
-export class NewExperimentComponent implements OnInit {
+export class NewExperimentComponent {
 
-  constructor() { }
+  profileForm = this.fb.group({
+    newname: ['', Validators.minLength(3)],
+    quantity: ['', Validators.required],
+    exp: this.fb.array([this.createExpItem()])
+  });
 
-  ngOnInit() {
+
+  constructor(private fb: FormBuilder) { }
+
+  createExpItem(): FormGroup {
+    return this.fb.group({
+      name: [''],
+      weight: [''],
+      id: [''],
+    });
+  }
+
+  get exp() {
+    return this.profileForm.get('exp') as FormArray;
+  }
+
+  addItem() {
+    this.exp.push(this.createExpItem());
+  }
+
+  onItemDeleted(index) {
+    this.exp.removeAt(index);
   }
 
 }
