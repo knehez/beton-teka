0109e7c6dd0c5e872d212f3ae3ac549f754bbcdb
Kiss, Aron <aron123dev@gmail.com>
2019-08-07 11:44:49 +0200

    Fix insert of experiment: fill measurement data with default value

--- src/backend/controllers/experiment.controller.ts
+++ src/backend/controllers/experiment.controller.ts
@@ -4,4 +4,23 @@ import { Experiment } from '../entities/experiment';
 
 export default class ExperimentCtrl extends BaseCtrl {
     model = getRepository(Experiment);
+
+    insert = async (req, res) => {
+        try {
+          const entity = this.model.create(req.body);
+
+          for (const measurement of entity['measurements']) {
+              measurement.measurementData = { data: [] };
+          }
+
+          await this.model.save(entity);
+
+          res.json({
+            success: true,
+            id: entity['id']
+          });
+        } catch (err) {
+          return this.handleError(res);
+        }
+      }
 }
