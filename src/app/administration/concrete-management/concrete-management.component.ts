d67e8454af6dab6ab1bfa9ece77fa08c16d59045
Karoly Nehez <aitnehez@uni-miskolc.hu>
2019-11-19 12:51:41 +0100

    concrete manager refactoring

--- src/app/administration/concrete-management/concrete-management.component.ts
+++ src/app/administration/concrete-management/concrete-management.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component} from '@angular/core';
 import { ConcreteService } from 'src/app/_services/concrete.service';
-import { Concrete } from 'src/backend/entities/concrete';
 import { MessageService } from 'primeng/api';
 import { GeneralRestService } from 'src/app/_services/general-rest.service';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 // import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -11,79 +10,59 @@ import { FormBuilder } from '@angular/forms';
   templateUrl: './concrete-management.component.html',
   styleUrls: ['./concrete-management.component.css']
 })
-export class ConcreteManagementComponent implements OnInit {
-  searchedConcreteName = '';
-  headColumns: any[];
-  concretePropertyColumns: any[];
+export class ConcreteManagementComponent {
+  searchName = '';
   concretes = [];
   selectedConcrete: any;
 
-  concreteForm = this.formBuilder.group({
-    selectedConcrete: [{
-      properties: []
-    }],
-    newConcreteData: this.createConcreteData()
+  headColumns = [
+    { field: 'id', header: 'Azonosító', hidden: false },
+    { field: 'label', header: 'Név', hidden: false },
+    { field: 'description', header: 'Leírás', hidden: false }
+  ];
+
+  propertiesColumns = [
+    { field: 'name', header: 'Név', hidden: false },
+    { field: 'value', header: 'Érték', hidden: false }
+  ];
+
+  nameValueForm = this.fb.group({
+    name: ['', [Validators.required]],
+    value: ['', [Validators.required]]
   });
 
-  constructor(
-    private concreteService: ConcreteService,
-    public restService: GeneralRestService,
-    public messageService: MessageService,
-    private formBuilder: FormBuilder
+  constructor(private concreteService: ConcreteService,
+    private restService: GeneralRestService,
+    private messageService: MessageService,
+    private fb: FormBuilder
   ) { }
 
-  ngOnInit() {
-    this.headColumns = [
-      { field: 'id', header: 'Azonosító', hidden: false },
-      { field: 'label', header: 'Név', hidden: false },
-      { field: 'description', header: 'Leírás', hidden: false }
-    ];
-
-    this.concretePropertyColumns = [
-      { field: 'name', header: 'Név', hidden: false },
-      { field: 'value', header: 'Érték', hidden: false }
-    ];
-  }
-
-  get selectedConcreteData() {
-    return this.concreteForm.get('selectedConcrete').value;
-  }
-
-  createConcreteData() {
-    return this.formBuilder.group({
-      name: [''],
-      value: ['']
-    });
-  }
-
   onConcreteSelect(event) {
-    this.concreteForm.get('selectedConcrete').setValue(event.data);
+    this.selectedConcrete = event.data;
+    // remove all old values
+    this.nameValueForm.reset();
   }
 
-  searchConcrete() {
-    this.concreteService.searchConcretes(this.searchedConcreteName)
+  search() {
+    this.concreteService.searchConcretes(this.searchName)
       .then(res => {
         this.concretes = res['data'] as [];
       });
   }
 
   addConcreteData() {
-    const data = this.concreteForm.get('newConcreteData').value;
-
-    this.selectedConcreteData.properties.push(data);
-    this.concreteForm.setControl('newConcreteData', this.createConcreteData());
+    this.selectedConcrete.properties.push(this.nameValueForm.value);
+    this.nameValueForm.reset();
     this.saveConcrete();
   }
 
   deleteRow(index) {
-    this.selectedConcreteData.properties.splice(index, 1);
+    this.selectedConcrete.properties.splice(index, 1);
     this.saveConcrete();
   }
 
   saveConcrete() {
-    const concreteToSave = this.selectedConcreteData;
-
-    this.concreteService.saveConcrete(concreteToSave)
+    this.concreteService.saveConcrete(this.selectedConcrete)
       .then(res => {
         this.messageService.add({
           severity: 'success',
